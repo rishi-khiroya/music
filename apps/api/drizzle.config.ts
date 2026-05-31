46fecf2daf64ffacc2c2ee994df1b9195edb0321
Rishi Khiroya <rishi@vrish.dev>
2026-05-31 18:55:36 +0100

    feat(api): Manually set db password from env for drizzle

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -4,9 +4,14 @@ import { defineConfig } from "drizzle-kit";
 config({ path: ".env.development" });
 
 const databaseUrl = process.env.DATABASE_URL;
+const password = process.env.PGPASSWORD;
 
 if (!databaseUrl) {
   throw new Error("DATABASE_URL is required");
+}
+
+if (!password) {
+  throw new Error("PGPASSWORD is required");
 }
 
 export default defineConfig({
@@ -15,6 +20,7 @@ export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
     url: databaseUrl,
+    password,
   },
   strict: true,
   verbose: true,
